89a625f71e63fdf3c44c655928cf35553e0e1103
Sonia Blanco <soniab@timedatasecurity.ie>
2024-02-28 16:50:48 +0000

    new rule

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 //TASMOTA RULE
-//Rule1 on SerialReceived#Data$<@MQTT@ do serialsend %value% endon
+//Rule1 on SerialReceived#? do Publish mbmqtt %value% endon
+
+
+//Rule1 ON SerialReceived#counter DO Mem1 %value% ENDON  ON SerialReceived#name DO Publish mbmqtt {"From":"%Mem1%","To":"%value%"}  ENDON  
